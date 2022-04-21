28addb61787d1521c306d5b9a5ccb94bd63985df
adnan-mujagic <adnanmujagic@outlook.com>
2022-04-22 01:47:27 +0200

    Cleaning up

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,5 @@
+const Product = require("../models/productModel");
+
 module.exports.createProduct = async (req, res) => {};
 
 module.exports.updateProduct = async (req, res) => {};
--- entityFactory.js
+++
@@ -1,7 +0,0 @@
-const entityFactory = (entity, requestBody) => {
-  Object.keys(requestBody).forEach((key) => {
-    entity[key] = requestBody[key];
-  });
-};
-
-module.exports.entityFactory = entityFactory;
