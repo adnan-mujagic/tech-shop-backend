dd38a458c38ee54229aded814bedc306acc7d382
adnan-mujagic <adnanmujagic@outlook.com>
2022-05-07 21:03:57 +0200

    Testing a failed test result

--- test/testTest.js
+++ test/testTest.js
@@ -4,4 +4,8 @@ describe("Testing tests", () => {
   it("Tests if 0 is equal to 0", () => {
     assert.equal(0, 0);
   });
+
+  it("Test should fail", () => {
+    assert.equal("Amar", "Ado");
+  });
 });
