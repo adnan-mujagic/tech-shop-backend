683ba4e995dc6967706bc588d2590d7d2a758183
adnan-mujagic <adnanmujagic@outlook.com>
2022-05-11 23:27:14 +0200

    Getting product and multiple products with basic pagination support

--- controllers/productController.js
+++ controllers/productController.js
@@ -24,6 +24,40 @@ module.exports.createProduct = async (req, res) => {
   });
 };
 
+module.exports.getProduct = async (req, res) => {
+  Product.findOne({ _id: req.params.product_id }).exec((err, product) => {
+    if (err) {
+      return res.status(404).json({
+        message: "Product could not be found",
+      });
+    }
+    return res.status(200).json({
+      message: "Product found successfully",
+      data: product,
+    });
+  });
+};
+
+module.exports.getProducts = async (req, res) => {
+  const { page = 1, pageSize = 25 } = req.query;
+  Product.find({})
+    .skip((page - 1) * pageSize)
+    .limit(pageSize)
+    .exec((err, products) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Something went wrong while fetching products",
+        });
+      }
+      return res.status(200).json({
+        message: "Products successfully fetched",
+        page,
+        pageSize,
+        data: products,
+      });
+    });
+};
+
 module.exports.updateProduct = async (req, res) => {
   if (!verifyAuthorization(req.headers, "ADMIN")) {
     return res.status(403).json({
--- routes/prouductRoutes.js
+++ routes/prouductRoutes.js
@@ -2,12 +2,18 @@ const {
   createProduct,
   deleteProduct,
   updateProduct,
+  getProduct,
+  getProducts,
 } = require("../controllers/productController");
 
 const router = require("express").Router();
 
-router.route("/").post(createProduct);
+router.route("/").get(getProducts).post(createProduct);
 
-router.route("/:product_id").put(updateProduct).delete(deleteProduct);
+router
+  .route("/:product_id")
+  .get(getProduct)
+  .put(updateProduct)
+  .delete(deleteProduct);
 
 module.exports = router;
